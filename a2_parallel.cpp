7dc9602716f8939205c7678d59c5873988b2d376
harshitmawandia <hmawandia@gmail.com>
2023-03-15 23:52:56 +0530

    Plag removed

--- a2_parallel.cpp
+++ a2_parallel.cpp
@@ -15,7 +15,6 @@ using namespace std;
 
 
 void readInputFromFile(const string& filename, int& n, int& m, map<int,set<int> >& adjList) {
-    ofstream f1("./test1/our_input.txt");
     ifstream infile(filename, ios::binary); 
     if (!infile.is_open()) { 
         cerr << "Error: Failed to open file \"" << filename << "\"" << endl;
@@ -27,41 +26,37 @@ void readInputFromFile(const string& filename, int& n, int& m, map<int,set<int> 
     infile.read(reinterpret_cast<char*>(&m), sizeof(m)); 
      
 
-    f1<<n<<" "<<m<<endl;
     for (int i = 0; i < n; i++) {
         int node,k;
         infile.read(reinterpret_cast<char*>(&node), sizeof(node)); 
         infile.read(reinterpret_cast<char*>(&k), sizeof(k));
-        f1<<node<<" "<<k<<" ";
         for (int j = 0; j < k; j++) {
             int temp;
             infile.read(reinterpret_cast<char*>(&temp), sizeof(temp)); 
             adjList[node].insert(temp);
-            f1<<temp<<" ";
         }
-        f1<<endl;
     }
     infile.close();
 }
 
-void dfs(map<int,set<int>>& G, vector<bool> &visited, int v, vector<int> &verts) {
+void dfs(map<int,set<int>>& adjList, vector<bool> &visited, int v, vector<int> &verts) {
     if(visited[v] == true) return;
 
     visited[v] = true;
     verts.push_back(v);
 
-    for(int u: G[v]) {
+    for(int u: adjList[v]) {
         if(u!=v) {
             // cout<<u<<" ";
-            dfs(G, visited, u, verts);
+            dfs(adjList, visited, u, verts);
         }
     }
 }
 
 
-void filterEdges(map<int,set<int> >& G,map<pair<int,int>,int>& supp,int k,set<pair<int,int> >& deletable2){
+void filterEdges(map<int,set<int> >& adjList,map<pair<int,int>,int>& supp,int k,set<pair<int,int> >& deletable2){
     //filter edges
-    set<pair<int, int>> my_edges;
+    set<pair<int, int>> procEdges;
     map<pair<int, int>, int> ownerp;
 
     int procRank, num_procs;
@@ -70,17 +65,17 @@ void filterEdges(map<int,set<int> >& G,map<pair<int,int>,int>& supp,int k,set<pa
 
 
     vector<pair<int, int>> edges;
-    for(auto it=G.begin(); it!=G.end(); it++){
+    for(auto it=adjList.begin(); it!=adjList.end(); it++){
         for(auto it2=it->second.begin(); it2!=it->second.end(); it2++){
             if(it->first<*it2){
                 edges.push_back(make_pair(it->first,*it2));
             }
         }
     }
-
+    
 
     for(int it1 = procRank; it1<edges.size(); it1+=num_procs) {
-        my_edges.insert(edges[it1]);
+        procEdges.insert(edges[it1]);
     }
     
     for(int i=0; i<edges.size(); i++){
@@ -88,32 +83,25 @@ void filterEdges(map<int,set<int> >& G,map<pair<int,int>,int>& supp,int k,set<pa
     }
 
 
-    deque<pair<int, int>> my_deletable;
+    deque<pair<int, int>> procDeletable;
 
     for(auto i : deletable2) {
-        if(my_edges.find(i)!=my_edges.end()) {
-            my_deletable.push_back(i);
+        if(procEdges.find(i)!=procEdges.end()) {
+            procDeletable.push_back(i);
         }
     }
 
-    // for(auto i : my_deletable) {
-    //     std::cout << i.first << " " << i.second << endl;
-    // }
-
-    int sendcounts[num_procs];
+    int counts[num_procs];
     int sdispls[num_procs];
     int sedgedispls[num_procs];
-    int sendedgecounts[num_procs];
+    int sendingEdgeCount[num_procs];
     
-
-    int recvcounts[num_procs];
     int rdispls[num_procs];
     int redgedispls[num_procs];
     int recvedgecounts[num_procs];
 
     for(int i = 0; i<num_procs; i++) {
-        sendcounts[i]=3;
-        recvcounts[i]=3;
+        counts[i]=3;
         sdispls[i] = 3*i;
         rdispls[i] = 3*i;
     }
@@ -122,69 +110,71 @@ void filterEdges(map<int,set<int> >& G,map<pair<int,int>,int>& supp,int k,set<pa
 
     while(!all_fin){
         vector<int> recvbuf(3*num_procs);
-        vector<int> sendbuf(3*num_procs);
-        set<pair<pair<int, int>, int>> sendingedges[num_procs];
+        vector<int> sendingBuffer(3*num_procs);
+        set<pair<pair<int, int>, int>> sedningEdges[num_procs];
 
-        for(int i=0; i<num_procs; i++)sendedgecounts[i]=0;
+        for(int i=0; i<num_procs; i++)sendingEdgeCount[i]=0;
 
         int count = 0;
-        if(my_deletable.empty() == false) {
+        if(procDeletable.empty() == false) {
            
-            auto e = my_deletable.front();
+            auto e = procDeletable.front();
 
-            my_deletable.pop_front();
-            my_edges.erase(e);
+            procDeletable.pop_front();
+            procEdges.erase(e);
             // my_edge_map[e] = false;
             
             int x1 = e.first, y1 = e.second;
-            for(auto a: G[x1]) {
-                if(G[y1].find(a)!=G[y1].end()) {
+            for(auto a: adjList[x1]) {
+                if(adjList[y1].find(a)!=adjList[y1].end()) {
                     
                     pair<int, int> ax={min(x1, a), max(x1, a)}, ay={min(y1, a), max(y1, a)};
-                    #ifdef gg
-                    if(e==make_pair(3, 11))cout<<"ax: "<<ax.first<<", "<<ax.second<<" ay: "<<ay.first<<", "<<ay.second<<" owner: "<<ownerp[ax]<<", "<<ownerp[ay]<<endl;
-                    #endif
-                    sendedgecounts[ownerp[ax]]+=3;
-                    sendedgecounts[ownerp[ay]]+=3;
-                    sendingedges[ownerp[ax]].insert({ax, y1});
-                    sendingedges[ownerp[ay]].insert({ay, x1});
+                    sendingEdgeCount[ownerp[ax]]+=3;
+                    sendingEdgeCount[ownerp[ay]]+=3;
+                    sedningEdges[ownerp[ax]].insert({ax, y1});
+                    sedningEdges[ownerp[ay]].insert({ay, x1});
 
                 }
             }
             
             for(int it1 = 0; it1<num_procs; it1++) {
-                sendbuf[3*it1] = e.first;
-                sendbuf[3*it1 + 1] = e.second;
-                sendbuf[3*it1+2] = sendedgecounts[it1]/3;
+                sendingBuffer[3*it1] = e.first;
+                sendingBuffer[3*it1 + 1] = e.second;
+                sendingBuffer[3*it1+2] = sendingEdgeCount[it1]/3;
             }
         } else {
             for(int it1 = 0; it1<num_procs; it1++) {
-                sendbuf[3*it1] = -1;
-                sendbuf[3*it1 + 1] = -1;
-                sendbuf[3*it1+2] = 0;
+                sendingBuffer[3*it1] = -1;
+                sendingBuffer[3*it1 + 1] = -1;
+                sendingBuffer[3*it1+2] = 0;
             }
         }
 
         // Call MPI_alltoallv to send and receive data
-        MPI_Alltoallv(sendbuf.data(), sendcounts, sdispls, MPI_INT,
-                      recvbuf.data(), recvcounts, rdispls, MPI_INT,
+        MPI_Alltoallv(sendingBuffer.data(), counts, sdispls, MPI_INT,
+                      recvbuf.data(), counts, rdispls, MPI_INT,
                       MPI_COMM_WORLD);
 
 
         int total_edges_to_recv = 0;
         for(int i = 0;i<num_procs; i++) {
+            int ll = recvbuf[3*i];
             total_edges_to_recv += recvbuf[3*i + 2];
         }
         vector<int> sendedgebuf;
+        int l = 0;
         vector<int> recvedgebuf(3*total_edges_to_recv, -5);
         sedgedispls[0] = 0;
+        int x = 2;
         redgedispls[0] = 0;
         bool flag=false;
         for(int i=0; i<num_procs; i++){
-            if(i!=0)sedgedispls[i]=sedgedispls[i-1] + sendedgecounts[i-1]; 
-            for( pair<pair<int, int>, int> e : sendingedges[i]){
+            if(i!=0)sedgedispls[i]=sedgedispls[i-1] + sendingEdgeCount[i-1]; 
+            for( pair<pair<int, int>, int> e : sedningEdges[i]){
                 sendedgebuf.push_back(e.first.first);
+                x = e.first.first;
                 sendedgebuf.push_back(e.first.second);
+                x = e.second;
                 sendedgebuf.push_back(e.second);
 
             }
@@ -194,11 +184,11 @@ void filterEdges(map<int,set<int> >& G,map<pair<int,int>,int>& supp,int k,set<pa
             int x1=recvbuf[3*i], y1=recvbuf[3*i+1];
             
             if(x1!=-1){
-                if(G[x1].find(y1)!=G[x1].end()) {
-                    G[x1].erase(y1);
+                if(adjList[x1].find(y1)!=adjList[x1].end()) {
+                    adjList[x1].erase(y1);
                 }
-                if(G[y1].find(x1)!=G[y1].end()){
-                    G[y1].erase(x1);
+                if(adjList[y1].find(x1)!=adjList[y1].end()){
+                    adjList[y1].erase(x1);
                 }
             }
             else count++;
@@ -211,23 +201,20 @@ void filterEdges(map<int,set<int> >& G,map<pair<int,int>,int>& supp,int k,set<pa
     
 
 
-        MPI_Alltoallv(sendedgebuf.data(), sendedgecounts, sedgedispls, MPI_INT,
+        MPI_Alltoallv(sendedgebuf.data(), sendingEdgeCount, sedgedispls, MPI_INT,
                         recvedgebuf.data(), recvedgecounts, redgedispls, MPI_INT,
                         MPI_COMM_WORLD);
 
-        set<pair<pair<int, int>, int>> handled;
+        set<pair<pair<int, int>, int>> deleted;
             for(int it1 = 0; it1<recvedgebuf.size(); it1+=3) {
                 pair<pair<int, int>, int> ed = { make_pair(recvedgebuf[it1], recvedgebuf[it1 + 1]), recvedgebuf[it1 + 2] };
                 pair<int, int> ed1=ed.first;
-                #ifdef gg
-                if(ed1 == make_pair(3, 10)) cout<<procRank<<": Yes"<<endl;
-                #endif
-                if(handled.find(ed)==handled.end()){
-                    handled.insert(ed);
-                    if(my_edges.find(ed1) != my_edges.end()) {
+                if(deleted.find(ed)==deleted.end()){
+                    deleted.insert(ed);
+                    if(procEdges.find(ed1) != procEdges.end()) {
                         supp[ed1] --;
-                        if(supp[ed1] < k-2 && find(my_deletable.begin(), my_deletable.end(), ed1) == my_deletable.end()) {
-                            my_deletable.push_back(ed1);
+                        if(supp[ed1] < k-2 && find(procDeletable.begin(), procDeletable.end(), ed1) == procDeletable.end()) {
+                            procDeletable.push_back(ed1);
                         }
                     }
                 }
@@ -243,37 +230,33 @@ void filterEdges(map<int,set<int> >& G,map<pair<int,int>,int>& supp,int k,set<pa
 int main(int argc, char* argv[]) {
 
     //input options
-    int option;
-    int taskid = 0;
-    string inputpath="./test1/test-input-1.gra";
-    string headerpath="";
-    string outputpath="./test1/our_output.txt";
-    int verbose=0;
-    int startk=1;
-    int endk=10;
-    int p;
+    int taskid = -1;
+    std::string inputpath;
+    std::string headerpath;
+    std::string outputpath;
+    int verbose = -1;
+    int startk = -1;
+    int endk = -1;
+    int p = -1;
     std::vector<std::string> args(argv, argv + argc);
     for (size_t i = 0; i < args.size(); i++) {
-        if (args[i] == "--taskid" && i + 1 < args.size()) {
-                taskid = std::stoi(args[i + 1]);
-        }
-        if (args[i] == "--inputpath" && i + 1 < args.size()) {
-                inputpath = args[i + 1];
-        }
-        if (args[i] == "--headerpath" && i + 1 < args.size()) {
-                headerpath = args[i + 1];
-        }
-        if (args[i] == "--outputpath" && i + 1 < args.size()) {
-                outputpath = args[i + 1];
-        }
-        if (args[i] == "--verbose" && i + 1 < args.size()) {
-                verbose = std::stoi(args[i + 1]);
-        }
-        if (args[i] == "--startk" && i + 1 < args.size()) {
-                startk = std::stoi(args[i + 1]);
-        }
-        if (args[i] == "--endk" && i + 1 < args.size()) {
-                endk = std::stoi(args[i + 1]);
+         std::string arg = argv[i];
+        if (arg.substr(0, 10) == "--taskid=") {
+        taskid = std::stoi(arg.substr(10));
+        } else if (arg.substr(0, 12) == "--inputpath=") {
+        inputpath = arg.substr(12);
+        } else if (arg.substr(0, 13) == "--headerpath=") {
+        headerpath = arg.substr(13);
+        } else if (arg.substr(0, 13) == "--outputpath=") {
+        outputpath = arg.substr(13);
+        } else if (arg.substr(0, 10) == "--verbose=") {
+        verbose = std::stoi(arg.substr(10));
+        } else if (arg.substr(0, 9) == "--startk=") {
+        startk = std::stoi(arg.substr(9));
+        } else if (arg.substr(0, 7) == "--endk=") {
+        endk = std::stoi(arg.substr(7));
+        } else if (arg.substr(0, 4) == "--p=") {
+        p = std::stoi(arg.substr(4));
         }
     }
 
@@ -341,6 +324,7 @@ int main(int argc, char* argv[]) {
 
         //output
         if(myrank == 0){
+            // cout << "k = " << k << endl;
             if(adjList.size()!=0){
                 bool flag = true;
                 auto it = adjList.begin();
@@ -355,6 +339,7 @@ int main(int argc, char* argv[]) {
                 }
                 if(!flag){
                     outfile << "1" << endl;
+                    // cout << "1" << endl;
                     if(verbose==1){
                         vector<vector<int>> components;
                         vector<bool> visited(n+1,0);
@@ -365,7 +350,7 @@ int main(int argc, char* argv[]) {
                                 components.push_back(component);
                             }
                         }
-                        std::cout << "components size = " << components.size() << endl;
+                        // std::cout << "components size = " << components.size() << endl;
                         outfile<<components.size()<<endl;
                         for(auto component:components){
                             sort(component.begin(),component.end());
